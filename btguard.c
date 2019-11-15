c4d4a86eb8a2bdf46d4f5ab3e38478367da6b66d
thinkycx <thinkycx@gmail.com>
2019-11-15 12:22:49 +0800

    update btguard.c: add log record

--- btguard.c
+++ btguard.c
@@ -11,7 +11,15 @@
         gcc btguard.c -o btguard -ldl
         MODE=0 ./btguard    // COMPLAIN mode, output: ./bt-canary.txt
         MODE=1 ./btguard    // RESTRICT mode, use the ./bt-canary.txt
- 
+ * Output:
+        [COMPLAIN] TIME: 1573791101s 	 BackTrace: 0x400c23 0x4010fb 0x7f15e25cd830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [+] NEW
+        [COMPLAIN] TIME: 1573791108s 	 BackTrace: 0x400c23 0x4010fb 0x7f1a1d74b830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [=] NUM 0
+        [COMPLAIN] TIME: 1573791112s 	 BackTrace: 0x400c23 0x4010fb 0x7f6caf268830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [=] NUM 0
+        [COMPLAIN] TIME: 1573791132s 	 BackTrace: 0x400c23 0x4010fb 0x7f96af439830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [=] NUM 0
+        [RESTRICT] TIME: 1573791138s 	 BackTrace: 0x400c23 0x4010fb 0x7f0bec6de830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [=] NUM 0
+        [COMPLAIN] TIME: 1573791151s 	 BackTrace: 0x400c23 0x4010fb 0x7faf038ba830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [=] NUM 0
+        [RESTRICT] TIME: 1573791162s 	 BackTrace: 0x400c23 0x4010fb 0x7fd03933f830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [=] NUM 0
+        [COMPLAIN] TIME: 1573791175s 	 BackTrace: 0x400c23 0x4010fb 0x7f19932ea830 0x4009e9 (nil) 	 BT_CANARY: 0x1f37 	 [=] NUM 0
  * */
 
 #include <stdio.h>
@@ -22,10 +30,13 @@
 #include <string.h>
 #include <unistd.h>     // open() read() write()   
 #include <dlfcn.h>      // dlopen() dlsym()
+#include <sys/time.h>   // gettimeofday()
 
 
 #define BT_BUF_SIZE 20
 #define MAX_BT_CANARY_NUM 8192
+#define BT_CANARY_NAME "bt-canary.txt"
+#define LOG_NAME "bt-canary.log"
 
 typedef int(*EXECVE)(const char*filename, char *const argv[], char *const envp[]);
 
@@ -49,7 +60,6 @@ int execve(const char* filename, char *const argv[], char *const envp[])
         handle = dlopen("libc.so.6", RTLD_LAZY);
         old_execve = (EXECVE)dlsym(handle, "execve");
     }
-    printf("hack function invoked. s1=<%s> \n", filename);
     
     char *pwd;                                                          // caculate the bt cannary                         
     long long buffer[BT_BUF_SIZE];
@@ -64,8 +74,8 @@ int execve(const char* filename, char *const argv[], char *const envp[])
     // printf("pwd %s\n", getenv("PWD"));
     memset(buffer, 0, BT_BUF_SIZE*sizeof(void *));
     backtrace((void *)buffer, BT_BUF_SIZE);                              // backtrace
-    sprintf(log_filename, "%s/%s", pwd, "bt-canary.txt");
-    printf("filename %s\n", log_filename);
+    sprintf(log_filename, "%s/%s", pwd, BT_CANARY_NAME);
+    // printf("filename %s\n", log_filename);
     
 
     //get bt canary                                                    // create if not exists
@@ -79,9 +89,34 @@ int execve(const char* filename, char *const argv[], char *const envp[])
     }
     read(fd, bt_canary_array, filesize);                        // read the content from the file
     
+    char log[500];                                              //record time and backtrace into the log_file
+    char log_name[255];
+    struct timeval tv;
+    int log_fd;
+
+    sprintf(log_name, "%s/%s", pwd, LOG_NAME);                      
+    log_fd = open(log_name, O_CREAT | O_RDWR | O_APPEND, S_IRWXU);  // open log_fd
+
+
+    if (env_mode != 0 && env_mode != 1)                          //default set to COMPLAIN 0
+        env_mode = 0;
+    
+    if (env_mode == 0)
+        write(log_fd, "[COMPLAIN] ", 11);
+    else if (env_mode ==1)
+        write(log_fd, "[RESTRICT] ", 11);
+
+    gettimeofday(&tv,NULL);
+    sprintf(log, "TIME: %lds \t BackTrace: ", tv.tv_sec);
+
+    write(log_fd, log, strlen(log));
+
     // sum backtrace
     for(int i=0; i<BT_BUF_SIZE; i++){                           // get each addr in the backtrace
-        printf("%p\n", (void *)buffer[i]);                      
+        // printf("%p\n", (void *)buffer[i]);      
+        sprintf(log, "%p ", (void *)buffer[i]);       
+        write(log_fd, log, strlen(log)); 
+
         buffer[i] = buffer[i] & 0xfff;                          // get the last 3 bit
         // write(fd, &buffer[i], 8);
         bt_canary += buffer[i]; 
@@ -90,30 +125,38 @@ int execve(const char* filename, char *const argv[], char *const envp[])
             break;
         }
     }
-
-    if (env_mode != 0 && env_mode != 1)                          //default set to COMPLAIN 0
-        env_mode = 0;
+    sprintf(log, "\t BT_CANARY: 0x%llx ", bt_canary);       
+    write(log_fd, log, strlen(log)); 
 
     // check whether the bt_canary is unique?
     for(int i=0; bt_canary_array[i] != 0; i++ ){
         if (bt_canary_array[i] == bt_canary){
-            if( env_mode == 0 )
-                printf("Equals to the num: %d , value: 0x%x \n", i, (int )bt_canary);
+                sprintf(log,"\t [=] NUM %d \n", i );    
+                write(log_fd, log, strlen(log)); 
             goto END;
         }
     }
     // NOT EQUALS! get new bt_canary!
     if (env_mode == 0){
-        printf("New bt_canary! write into the file...\n");
-        write(fd, &bt_canary, 8);
+        sprintf(log,"\t [+] NEW \n");
+        write(log_fd, log, strlen(log)); 
+
+        write(fd, &bt_canary, 8);                   // write new canary into the file.
     }else if(env_mode == 1){
-        printf("New bt_canary in restrict mode!\n");
+        sprintf(log,"\t [!] NEW \n");
+        write(log_fd, log, strlen(log)); 
         return -1;
     }
 
 END:
-    close(fd);
+    // record args if needed...
+    // sprintf(log,"EXECVE function invoked. filename: s1=<%s> \n", filename);
+    // write(log_fd, log, strlen(log)); 
+
     old_execve(filename, argv, envp);
+
+    close(fd);
+    close(log_fd);
 
     return 0;
 }
